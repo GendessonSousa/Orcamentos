ed998d8abbab57e90d14c9e68b2c3653fd46ba94
GendessonSousa <gendessonsousa078@gmail.com>
2025-10-30 17:35:18 -0300

    chore: inicializar projeto Spring Boot

---
+++ src/main/java/com/Gendesson/orcamentos/OrcamentosApplication.java
@@ -0,0 +1,13 @@
+package com.Gendesson.orcamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gendesson/orcamentos/OrcamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gendesson.orcamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
