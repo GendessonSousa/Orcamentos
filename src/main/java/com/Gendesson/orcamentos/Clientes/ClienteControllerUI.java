5e4a18913b2d63611ffaa7d765d9dcec6bdd59ab
GendessonSousa <gendessonsousa078@gmail.com>
2026-02-02 22:00:56 -0300

    feat: implementa UI para tabela clientes

---
+++ src/main/java/com/Gendesson/orcamentos/Clientes/ClienteControllerUI.java
@@ -0,0 +1,99 @@
+package com.Gendesson.orcamentos.Clientes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/clientes/ui")
+public class ClienteControllerUI {
+    private final ClienteService clienteService;
+
+    public ClienteControllerUI(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @GetMapping("/listar")
+    public String listarClientes (Model model){
+        List<ClienteDTO> clientes = clienteService.listarClientes();
+        model.addAttribute("clientes", clientes);
+        return "listarClientes";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarClientePorId(@PathVariable Long id){
+        clienteService.deletarClientes(id);
+        return "redirect:/clientes/ui/listar";
+    }
+
+    @GetMapping("/listar/{id}")
+    public String listarClientePorId(@PathVariable Long id, Model model){
+        ClienteDTO cliente = clienteService.listarClientesPorId(id);
+        if (cliente != null){
+            model.addAttribute("cliente", cliente);
+            return "detalhesCliente";
+        } else {
+            model.addAttribute("mensagem", "Cliente não encontrado!");
+            return "listarClientes";
+        }
+    }
+
+    @GetMapping("/adicionar")
+    public String formularioAdicionarCliente (Model model){
+        model.addAttribute("cliente", new ClienteDTO());
+        return "adicionarCliente";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarCliente (@ModelAttribute ClienteDTO cliente, RedirectAttributes redirectAttributes){
+        clienteService.criarCliente(cliente);
+        redirectAttributes.addFlashAttribute("mensagem", "Cliente cadastrado com sucesso!");
+        return "redirect:/clientes/ui/listar";
+    }
+
+
+    @GetMapping("/detalhes/{id}")
+    public String detalhesCliente(@PathVariable Long id, Model model) {
+        ClienteDTO cliente = clienteService.listarClientesPorId(id);
+
+        if (cliente == null) {
+            return "redirect:/clientes/ui/listar";
+        }
+
+        model.addAttribute("cliente", cliente);
+        return "detalhesCliente";
+    }
+
+
+    @GetMapping("/alterar/{id}")
+    public String alterarCliente(@PathVariable Long id, Model model) {
+        ClienteDTO cliente = clienteService.listarClientesPorId(id);
+
+        if (cliente == null) {
+            return "redirect:/clientes/ui/listar";
+        }
+
+        model.addAttribute("cliente", cliente);
+        return "alterarCliente";
+    }
+
+    @PostMapping("/alterar")
+    public String salvarAlteracaoCliente(@ModelAttribute ClienteDTO cliente,
+                                         RedirectAttributes redirectAttributes) {
+
+        clienteService.atualizarClientes(cliente.getId(), cliente);
+
+        redirectAttributes.addFlashAttribute(
+                "mensagem",
+                "Cliente atualizado com sucesso!"
+        );
+
+        return "redirect:/clientes/ui/listar";
+    }
+
+
+
+}
