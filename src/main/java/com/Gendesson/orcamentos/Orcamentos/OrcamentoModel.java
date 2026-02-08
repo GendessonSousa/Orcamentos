3eed8c920e75710a902c80693d58b1c4e5199cb1
GendessonSousa <gendessonsousa078@gmail.com>
2026-02-08 20:28:19 -0300

    refactor: refatora entidade Orcamento para suportar múltiplos itens e controle de valores

--- src/main/java/com/Gendesson/orcamentos/Orcamentos/OrcamentoModel.java
+++ src/main/java/com/Gendesson/orcamentos/Orcamentos/OrcamentoModel.java
@@ -1,8 +1,13 @@
 package com.Gendesson.orcamentos.Orcamentos;
 
 import com.Gendesson.orcamentos.Clientes.ClienteModel;
+import com.Gendesson.orcamentos.ItemOrcamento.ItemOrcamentoModel;
 import com.Gendesson.orcamentos.Servicos.ServicoModel;
 import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_orcamentos")
@@ -12,32 +17,74 @@ public class OrcamentoModel {
     private Long id;
     @ManyToOne
     @JoinColumn(name = "cliente_id")
-    private ClienteModel clienteModel;
-    @ManyToOne
-    @JoinColumn(name = "servico_id")
-    private ServicoModel servicoModel;
+    private ClienteModel cliente;
+    @OneToMany(mappedBy = "orcamento", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ItemOrcamentoModel> itens;
+
+    private BigDecimal valorTotal;
+
+    private LocalDate dataCriacao;
+
+    private LocalDate dataValidade;
+
 
     public OrcamentoModel() {
     }
 
-    public OrcamentoModel(ClienteModel clienteModel, ServicoModel servicoModel) {
-        this.clienteModel = clienteModel;
-        this.servicoModel = servicoModel;
+    public OrcamentoModel(Long id, ClienteModel cliente, List<ItemOrcamentoModel> itens, BigDecimal valorTotal, LocalDate dataCriacao, LocalDate dataValidade) {
+        this.id = id;
+        this.cliente = cliente;
+        this.itens = itens;
+        this.valorTotal = valorTotal;
+        this.dataCriacao = dataCriacao;
+        this.dataValidade = dataValidade;
     }
 
-    public ClienteModel getClienteModel() {
-        return clienteModel;
+    public Long getId() {
+        return id;
     }
 
-    public void setClienteModel(ClienteModel clienteModel) {
-        this.clienteModel = clienteModel;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public ServicoModel getServicoModel() {
-        return servicoModel;
+    public ClienteModel getCliente() {
+        return cliente;
     }
 
-    public void setServicoModel(ServicoModel servicoModel) {
-        this.servicoModel = servicoModel;
+    public void setCliente(ClienteModel cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<ItemOrcamentoModel> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemOrcamentoModel> itens) {
+        this.itens = itens;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDate getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(LocalDate dataValidade) {
+        this.dataValidade = dataValidade;
     }
 }
